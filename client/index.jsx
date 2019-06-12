460bba9da8f0c2e2afb43f94610eb52b38e5c90c
Tracy Yu <tracyyu93@g.ucla.edu>
2019-06-11 17:30:35 -0700

    Update repo file

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(<div>HI!</div>, document.getElementById('app'));
+ReactDOM.render(<div>HI! WELCOME to REACT WORKSHOP</div>, document.getElementById('app'));
